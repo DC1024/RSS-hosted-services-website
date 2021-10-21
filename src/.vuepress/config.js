dbc7f2d0b046ee357df87a0e6fcaafb4285cbced
DC <34479243+DC1024@users.noreply.github.com>
2021-10-21 21:58:16 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,40 +1,73 @@
 module.exports = {
   lang: 'zh-CN',
-  title: 'RSS托管服务',
+  title: '托管服务',
   description: 'Free try and cheap',
-  base: '/RSS-hosted-services-website/'
-  head: [['link', { rel: 'icon', href: 'https://git.tt-rss.org/repo-avatars/3-9e1f83d2fac71ea097eeaf1776137cde' }]],
+  head: [['link', { rel: 'icon', href: 'https://notion-avatar.vercel.app/api/svg/eyJmYWNlIjoxLCJub3NlIjozLCJtb3V0aCI6OSwiZXllcyI6NywiZXllYnJvd3MiOjAsImdsYXNzZXMiOjEwLCJoYWlyIjo2LCJhY2Nlc3NvcmllcyI6MCwiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjB9' }]],
 
   themeConfig: {
-    logo: 'https://git.tt-rss.org/repo-avatars/3-9e1f83d2fac71ea097eeaf1776137cde',
-    sidebar: [
-      // 侧栏
-      {
-        text: '什么是RSS',
-        link: '/guide/',
-      },
-      // 字符串 - 页面文件路径
-      '/guide/getuse.md',
-      '/guide/deployment.md',
-    ],
+    logo: 'https://notion-avatar.vercel.app/api/svg/eyJmYWNlIjoxLCJub3NlIjozLCJtb3V0aCI6OSwiZXllcyI6NywiZXllYnJvd3MiOjAsImdsYXNzZXMiOjEwLCJoYWlyIjo2LCJhY2Nlc3NvcmllcyI6MCwiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjB9',
+    // sidebar: [
+    // 全页面显示同一个侧栏
+    // 字符串 - 页面文件路径
+    //   '/rss/guide/',
+    //   '/rss/guide/getuse.md',
+    //  '/rss/guide/deployment.md',
+    // ],
+    // 侧边栏对象
+    // 不同子路径下的页面会使用不同的侧边栏
+    sidebar: {
+      '/rss/': [
+        {
+          text: 'RSS',
+          children: ['/rss/guide/README.md', '/rss/guide/getuse.md','/rss/guide/deployment.md'],
+        },
+      ],
+      '/reference/': [
+        {
+          text: 'Reference',
+          children: ['/reference/cli.md', '/reference/config.md'],
+        },
+      ],
+      '/maintain/': [
+        {
+          text: '维护明细',
+          children: ['/maintain/README.md'],
+        },
+      ],
+    },
+    
     navbar: [
       // 单个导航栏
     {
-        text: 'VuePress',
-        link: 'https://v2.vuepress.vuejs.org/zh/',
+        text: 'DCCHEN',
+        link: 'http://blog.dcchen.top/',
     },
     {
         text: 'Github',
         link: 'https://github.com/DC1024/RSS-hosted-services-website',
     },
-    {
-        text: 'DCCHEN',
-        link: 'http://blog.dcchen.top/',
-    },
        // 单个导航组
-    {
+      {
         text: '语言 / Language',
-        children: ['../', '/en/'],
+        children: ['../', '/en/', '/klingon/'],
+    },
+    {
+        text: '基于',
+        children: 
+            [
+                    {
+                        text: 'VuePress',
+                        link: 'https://v2.vuepress.vuejs.org/zh/',
+                    },
+                    {
+                        text: 'TinyTinyRSS',
+                        link: 'https://tt-rss.org/',
+                    },
+                    {
+                        text: 'ArchiSteamFarm',
+                        link: 'https://github.com/JustArchiNET/ArchiSteamFarm/',
+                    },
+            ],
     },
     ],
   },
